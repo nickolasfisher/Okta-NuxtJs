77be1b6e4cc3b778c0e4ef0b168dfb3802278a32
fishbowlsoftware <nik@fishbowlllc.com>
2020-08-31 16:40:37 -0400

    initial commit

---
+++ pages/dashboard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <b-table :items="posts" :fields="fields"> </b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  authenticated: true,
+  data() {
+    return {
+      fields: ['userId', 'title', 'body'],
+      posts: []
+    }
+  },
+  async fetch() {
+    this.$axios
+      .$get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+        this.posts = res       
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="page-content">
+    <b-jumbotron
+      style="margin-top: 5vh"
+      header="Lets Get Some Posts"
+      lead="A Simple App with Nuxt and Okta"
+    >
+      <p>For more information visit thier websites</p>
+      <b-button variant="primary" href="https://nuxtjs.org/" target="blank">Nuxt</b-button>
+      <b-button variant="outline-primary" href="https://www.okta.com/" target="blank">Okta</b-button>
+    </b-jumbotron>
+  </div>
+</template>
